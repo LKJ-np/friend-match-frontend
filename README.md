f897902bdb7025042ca3212de8b6d26d88603067
“LKJ” <531304212@qq.com>
2023-12-01 20:56:28 +0800

    前端总结

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 伙伴匹配系统
+# 项目启动 
+npm install
+npm run dev
+修改myAxios.ts，dev在本地，build在远程服务器
--- src/plugins/myAxios.ts
+++ src/plugins/myAxios.ts
@@ -3,7 +3,7 @@ import axios, {AxiosInstance} from "axios";
 const isDev = process.env.NODE_ENV === 'development';
 
 const myAxios: AxiosInstance = axios.create({
-    baseURL: isDev ? 'http://localhost:8081/api' : 'http://8.134.201.204:8081/api',
+    baseURL: isDev ? 'http://localhost:8081/api' : 'http://服务器地址:8081/api',
 });
 
 myAxios.defaults.withCredentials = true; // 配置为true
